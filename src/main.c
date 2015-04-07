d71e37796b0facba2010e30f70c21443e8405ca9
Vladimir <riuson@gmail.com>
2015-04-07 18:23:42 +0500

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : eeprom-emulation.c
+ Author      : riuson
+ Version     :
+ Copyright   : Chelyabinsk 2015
+ Description : EEPROM Emulation on Flash
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
