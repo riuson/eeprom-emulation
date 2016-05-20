f60d926f7607b277fde10e72fc7fa421e74b6ed8
Vladimir <riuson@gmail.com>
2016-05-20 20:16:05 +0500

    Fixed AStyle script under Windows

--- apply-style.sh
+++ apply-style.sh
@@ -1,2 +1,22 @@
-astyle --options=astyle.conf "./src/*.c*"
-astyle --options=astyle.conf "./src/*.h"
+#!/bin/sh
+PROJECT_DIR_PATH=$1
+
+ASTYLE=astyle
+
+if command -v $ASTYLE >/dev/null 2; then
+  echo "AStyle detected."
+else
+  echo "AStyle not found. Try included astyle.exe."
+
+  ASTYLE=$PROJECT_DIR_PATH/AStyle.exe
+
+  if command -v $ASTYLE >/dev/null 2; then
+    echo "AStyle detected at $ASTYLE. Try using it."
+  else
+    echo "AStyle not found. Aborting."
+    exit 1;
+  fi
+fi
+
+$ASTYLE --options=$PROJECT_DIR_PATH/astyle.conf "$PROJECT_DIR_PATH/src/*.c*"
+$ASTYLE --options=$PROJECT_DIR_PATH/astyle.conf "$PROJECT_DIR_PATH/src/*.h"
