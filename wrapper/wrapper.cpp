89d7f9c3997ff6bad0f757c148d5cdf1c2b14c91
Vladimir <riuson@gmail.com>
2016-05-20 20:37:05 +0500

    Added VC++ project to wrap code to dll

---
+++ wrapper/wrapper.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "wrapper.h"
+#include "../src/eeprom_emulation.h"
+
+WRAPPER_API int LIB_eeprom_init_debug(uint32_t flash_address, uint32_t words_on_page, uint32_t pages_count)
+{
+    return eeprom_init_debug(flash_address, words_on_page, pages_count);
+}
+
+WRAPPER_API int LIB_eeprom_read_value(uint16_t key, uint16_t *value)
+{
+    return eeprom_read_value(key, value);
+}
+
+WRAPPER_API int LIB_eeprom_write_value(uint16_t key, uint16_t value)
+{
+    return eeprom_write_value(key, value);
+}
