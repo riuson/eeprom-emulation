e701d8e1671608cae99c1b38f0a4588bf980fb23
Vladimir <riuson@gmail.com>
2016-05-20 21:29:15 +0500

    Added test for limited records count

--- testlib/Tests/Test.cs
+++ testlib/Tests/Test.cs
@@ -12,6 +12,7 @@ namespace testlib
         private const int AllocatedSize = 1024 * 1024;
         private const int WordsOnPage = 128;
         private const int PagesCount = 4;
+        private const int ReservedWords = 1;
 
         private void ShowContent()
         {
@@ -29,17 +30,31 @@ namespace testlib
         [Test]
         public void CanWriteValues()
         {
-            for (ushort i = 0; i < 50; i++)
+            ushort count = 100;
+
+            for (ushort i = 0; i < count; i++)
             {
                 Eeprom.Result result = Eeprom.WriteValue(i, i);
-
                 Assert.That(result, Is.EqualTo(Eeprom.Result.Success));
             }
         }
 
         [Test]
-        public void Test2()
+        public void CanWriteOnlyFittableCount([Range(0, WordsOnPage + 20)]int count)
         {
+            for (ushort i = 0; i < Convert.ToUInt16(count); i++)
+            {
+                Eeprom.Result result = Eeprom.WriteValue(i, i);
+
+                if (i < WordsOnPage - ReservedWords)
+                {
+                    Assert.That(result, Is.EqualTo(Eeprom.Result.Success));
+                }
+                else
+                {
+                    Assert.That(result, Is.EqualTo(Eeprom.Result.NoEmptyRecord));
+                }
+            }
         }
 
         [TearDown]
