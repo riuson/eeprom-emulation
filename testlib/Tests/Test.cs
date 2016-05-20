85bbdb171267acdd8c8cd3819a210b2737bec639
Vladimir <riuson@gmail.com>
2016-05-20 21:22:54 +0500

    Added first test for simple write operations

--- testlib/Tests/Test.cs
+++ testlib/Tests/Test.cs
@@ -13,25 +13,38 @@ namespace testlib
         private const int WordsOnPage = 128;
         private const int PagesCount = 4;
 
-        [OneTimeSetUp]
+        private void ShowContent()
+        {
+            this.mMemoryArray.Print(0, WordsOnPage * (PagesCount + 1) * sizeof(UInt32));
+        }
+
+        [SetUp]
         public void Pre()
         {
             this.mMemoryArray = new byte[AllocatedSize];
             Lib.EepromResult result = Lib.EepromInitDebug(this.mMemoryArray, WordsOnPage, PagesCount);
             Assert.That(result, Is.EqualTo(Lib.EepromResult.Success));
-
-            this.mMemoryArray.Print(0, WordsOnPage * (PagesCount + 1) * sizeof(UInt32));
         }
 
         [Test]
-        public void Test1()
+        public void CanWriteValues()
+        {
+            for (ushort i = 0; i < 50; i++)
+            {
+                Lib.EepromResult result = Lib.EepromWriteValue(i, i);
+
+                Assert.That(result, Is.EqualTo(Lib.EepromResult.Success));
+            }
+        }
+
+        [Test]
+        public void Test2()
         {
         }
 
-        [OneTimeTearDown]
+        [TearDown]
         public void Post()
         {
-
         }
     }
 }
